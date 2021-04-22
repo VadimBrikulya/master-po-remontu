c461d9f1ad0a6b0c0de6f395ed90fb4b98c127cf
Vadim Brikulya <im.master99@gmail.com>
2021-04-22 22:51:08 +0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 
 const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+
 
 function styles() {
 
     return src("app/scss/style.scss")
-        .pipe(scss())
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
         .pipe(dest('app/css'))
+};
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
 }
+
+exports.styles = styles;
+exports.watching = watching;
